f560c14f1e313c87292f8855ca45724275058a1b
Stella Demchenko <144791455+P1ik0@users.noreply.github.com>
2024-03-25 03:13:50 +0200

    ыаыа

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -2,9 +2,10 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 
 
-    @Entity (name = "users")
+@Entity (name = "users")
     public class User{
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,58 +21,48 @@ import jakarta.persistence.*;
         // Конструкторы
 
 
-        public Long getId() {
-            return id;
-        }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
+    public String getUsername() {
+        return username;
+    }
 
-        public String getUsername() {
-            return username;
-        }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-        public void setUsername(String username) {
-            this.username = username;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public String getPhone() {
-            return phone;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhone(String phone) {
+    }
 
-        public void setPhone(String phone) {
-            this.phone = phone;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-
-        public User(){
-
-        }
-        public User(Long id, String username, String phone, String email, String password) {
-            this.id = id;
-            this.username = username;
-            this.phone = phone;
-            this.email = email;
-            this.password = password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 
+
+    public User(){
+
+    }
+
+    public User(Long userId, String username, String email, String password, LocalDateTime createdAt) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+
+ ;
+    }
+
+
+}
 
--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,9 +4,7 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     User findByUsername(String username);
-    User findByEmail(String email);
-    User findByphone(String phone);
-
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
